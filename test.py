f649788c2110ecdb6da1ccb366375858c4576a82
crank-gig <129849467+crank-gig@users.noreply.github.com>
2025-11-14 10:46:15 +0100

    Initial push

---
+++ test.py
@@ -0,0 +1,17 @@
+def main():
+    range(0,10)
+
+
+def use_lambda(n):
+    return lambda x: x + n
+
+
+def add(x,y):
+    return x+y
+
+
+
+
+if __name__ == "__main__":
+    sum = add(1,3)
+    print(sum)
